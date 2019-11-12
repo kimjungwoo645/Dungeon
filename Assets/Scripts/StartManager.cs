bd6317867fdea93bf79ba0e3b1594b8d698015f6
Jeung Woo <odav12@naver.com>
2019-11-12 11:38:57 +0900

    게임시작 화면 제작완료 #1
    타이틀,시작버튼,종료버튼
    
    종료버튼은 확인 못해봄 빌드가 안됨

---
+++ Assets/Scripts/StartManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    public void GameStart()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+    public void GameClose()
+    {
+        Application.Quit();
+    }
+}
