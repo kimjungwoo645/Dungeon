b1ab84b235176b0aa99598026583574a0c6caf70
Jeung Woo <odav12@naver.com>
2019-11-12 14:57:57 +0900

    #3 일부완성
    미니맵 띄우기 완성
    남은것
    레벨,체력,마나 표시하기

---
+++ Assets/Scripts/MapManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapManager : MonoBehaviour
+{
+    [SerializeField] Sprite[] sprite;
+    [SerializeField] Image map;
+
+    private void Awake()
+    {
+        sprite = Resources.LoadAll<Sprite>("maps");
+
+        map.sprite = sprite[Random.Range(1,5)];
+    }
+}
