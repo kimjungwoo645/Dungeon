d537c8de1bbd5bd7c347494a0d3f0963aed6a370
Jeung Woo <odav12@naver.com>
2019-11-13 09:47:34 +0900

    #3 인벤토리 일부완성
    OnOff기능구현
    남은거
    장비창,인벤토리칸

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    bool OnOff = true;
+
+    public GameObject inventory;
+
+    private void Start()
+    {
+        inventory.SetActive(false);
+    }
+
+    public void InventoryOnOff()
+    {
+        if(OnOff)
+        {
+            inventory.SetActive(true);
+            OnOff = !OnOff;
+        }
+        else if(!OnOff)
+        {
+            inventory.SetActive(false);
+            OnOff = !OnOff;
+        }
+    }
+}
